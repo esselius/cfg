1289f76cf0ec4d1037ba314e328226f3d2d7ff62
Peter Esselius <pepp@me.com>
2024-07-09 08:03:26 +0200

    test unstable for linux-builder, more specific

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
 
     raspberry-pi-nix.url = "github:tstat/raspberry-pi-nix";
     authentik-nix.url = "github:nix-community/authentik-nix/node-22";
