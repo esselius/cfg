d8110d83a4c700fb15e643ae744579d4e7262f57
Peter Esselius <pepp@me.com>
2024-07-26 11:21:34 +0200

    Run test from inside vm

--- nixos-tests/monitoring-auth.nix
+++ nixos-tests/monitoring-auth.nix
@@ -3,16 +3,12 @@
 {
   name = "monitoring-auth";
 
-  nodes.machine = {
+  nodes.machine = { pkgs, ... }: {
     _module.args.mkAuthentikScope = inputs.authentik-nix.lib.mkAuthentikScope;
 
     virtualisation = {
-      cores = 3;
-      memorySize = 4096;
-      forwardPorts = [
-        { host.port = 3000; guest.port = 3000; }
-        { host.port = 9000; guest.port = 9000; }
-      ];
+      cores = 6;
+      memorySize = 8192;
     };
 
     imports = [
@@ -84,11 +80,59 @@
         name = "Authentik";
         client_id_file = builtins.toFile "grafana-client-id" "grafana";
         client_secret_file = builtins.toFile "grafana-client-secret" "secret";
-        auth_url = "http://127.0.0.1:9000/application/o/authorize/";
-        token_url = "http://127.0.0.1:9000/application/o/token/";
-        api_url = "http://127.0.0.1:9000/application/o/userinfo/";
+        auth_url = "http://localhost:9000/application/o/authorize/";
+        token_url = "http://localhost:9000/application/o/token/";
+        api_url = "http://localhost:9000/application/o/userinfo/";
       };
     };
+
+    environment.sessionVariables = {
+      PLAYWRIGHT_BROWSERS_PATH = pkgs.playwright-driver.browsers;
+    };
+
+    environment.systemPackages = [
+      (pkgs.writers.writePython3Bin "test_auth" {
+        libraries = [pkgs.python3Packages.playwright];
+      } ''
+        from playwright.sync_api import sync_playwright, expect
+
+        with sync_playwright() as p:
+            browser = p.chromium.launch()
+
+            context = browser.new_context()
+            context.set_default_timeout(30000)
+            context.tracing.start(screenshots=True, snapshots=True)
+            page = context.new_page()
+
+            try:
+                print("Login page")
+                page.goto("http://localhost:3000/login")
+                # page.reload()
+                page.get_by_role("link", name="Sign in with Authentik").click()
+
+                print("Enter username")
+                page.get_by_placeholder("Email or Username").fill("akadmin")
+                page.get_by_role("button", name="Log in").click()
+
+                # page.reload()
+                print("Enter password")
+                page.get_by_placeholder("Please enter your password").fill("password")
+                page.get_by_role("button", name="Continue").click()
+
+                print("Consent page")
+                page.get_by_role("button", name="Continue").click()
+
+                print("Grafana landing page")
+                x = expect(page.get_by_role("heading", name="Starred dashboards"))
+                x.to_be_visible()
+            except Exception as e:
+                raise e
+            finally:
+                context.tracing.stop(path="/tmp/trace.zip")
+                context.close()
+                browser.close()
+      '')
+    ];
   };
 
   extraPythonPackages = p: [ p.playwright ];
@@ -110,28 +154,12 @@
     with subtest("Wait for Authentik blueprints to be applied"):
       machine.wait_until_succeeds("curl -f http://localhost:9000/application/o/grafana/.well-known/openid-configuration >&2")
 
-    from playwright.sync_api import sync_playwright, expect
+    with subtest("Test auth"):
+      ret, output = machine.execute("test_auth")
+      print(output)
 
-    with sync_playwright() as p:
-      browser = p.chromium.launch()
-      page = browser.new_page()
-      page.set_default_timeout(30000)
-
-      with subtest("Login page"):
-        page.goto("http://localhost:3000/login")
-        page.reload()
-        page.get_by_role("link", name="Sign in with Authentik").click()
-      with subtest("Enter username"):
-        page.get_by_placeholder("Email or Username").fill("akadmin")
-        page.get_by_role("button", name="Log in").click()
-      with subtest("Enter password"):
-        page.get_by_placeholder("Please enter your password").fill("password")
-        page.get_by_role("button", name="Continue").click()
-      with subtest("Consent page"):
-        page.get_by_role("button", name="Continue").click()
-      with subtest("Grafana landing page"):
-        expect(page.get_by_role("heading", name="Starred dashboards")).to_be_visible(timeout=30000)
-
-      browser.close()
+      if ret != 0:
+        machine.copy_from_vm("/tmp/trace.zip", ".")
+        exit(1)
   '';
 }
