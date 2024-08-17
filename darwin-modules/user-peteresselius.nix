e9508d5a8fd0838925d45f017521a248f4d4f37f
Peter Esselius <pepp@me.com>
2024-08-17 10:59:27 +0200

    ipad ssh key

--- darwin-modules/user-peteresselius.nix
+++ darwin-modules/user-peteresselius.nix
@@ -7,6 +7,7 @@
     shell = pkgs.fish;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFMdasDSm/XlOpv15asMENnQ/E9W9rXExBcUAVd/G6Mo"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKSArp+2Vu/AgbaiFYRLH/gtENAqwd6/aPVwgX429Tk+"
     ];
   };
 
--- nixos-modules/user-peteresselius.nix
+++ nixos-modules/user-peteresselius.nix
@@ -8,6 +8,7 @@
     shell = pkgs.fish;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFMdasDSm/XlOpv15asMENnQ/E9W9rXExBcUAVd/G6Mo"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKSArp+2Vu/AgbaiFYRLH/gtENAqwd6/aPVwgX429Tk+"
     ];
     hashedPassword = "$y$j9T$E9zqN0.bUa0F8DlygXLw61$7zSyujQLTey76rr.BzD4LVRzGXXP9gKTUmP.iOkXxfA";
   };
