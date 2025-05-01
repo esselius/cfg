3148566a77dc9edf71d88c295bc286692bc3c4ee
Peter Esselius <pepp@me.com>
2025-05-01 22:34:35 +0200

    drop unused age secret

--- home-configurations/peteresselius.nix
+++ home-configurations/peteresselius.nix
@@ -13,11 +13,6 @@
   ];
 
   config = {
-    age.identityPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
-    age.secrets.email.file = ../secrets/${config.context}-email.age;
-    programs.fish.shellInit = ''
-      set -x EMAIL (sh -c 'cat ${config.age.secrets.email.path}')
-    '';
     context = "home";
   };
 }
